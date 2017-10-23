80f7fac98a1b8824a9af950263d6f8e14c894f5f
Shannon McCormack <shannonkmc@hotmail.com>
2017-10-23 20:10:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Write your code in this file!
 function scuberGreetingForFeet () {
-  let feet;
     if (feet <= 400) {
       return = 'This one is on me!';
     } else if (1999 < feet && feet < 2500) {
