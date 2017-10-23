06f558288ac209d33f31c301b92ef66e163969c9
Shannon McCormack <shannonkmc@hotmail.com>
2017-10-23 20:24:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,11 @@ function ternaryCheckCity (city) {
 function switchOnCharmFromTip (tip) {
   switch (tip) {
     case "generous":
-    return "Thank you so much.";
+      return "Thank you so much.";
     case "not as generous":
-    return "Thank you.";
+      return "Thank you.";
+    default:
+      return "Bye.";
   }
 
 }
